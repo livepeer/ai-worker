e9b7ed62de63c4cbb6626bc8019997c7ba1ff659
Yondon Fu <yondon.fu@gmail.com>
2023-12-27 21:43:14 +0000

    jobs/containers/svd-xt: Fix predict

--- jobs/containers/svd-xt/predict.py
+++ jobs/containers/svd-xt/predict.py
@@ -2,6 +2,7 @@ from cog import BasePredictor, Input, File, Path
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import export_to_video
 import torch
+from PIL import Image
 
 class Predictor(BasePredictor):
   def setup(self):
@@ -11,12 +12,12 @@ class Predictor(BasePredictor):
 
   def predict(
     self,
-    image: File = Input(description="Image to guide generation"),
+    image: Path = Input(description="Image to guide generation"),
     motion_bucket_id: float = Input(description="The motion bucket ID. Used as conditioning for the generation. The higher the number the more motion will be in the video", default=127),
     noise_aug_strength: float = Input(description="The amount of noise added to the init image, the higher it is the less the video will look like the init image. Increase it for more motion", default=0.02)
   ) -> Path:
     generator = torch.manual_seed(42)
-    frames = self.pipeline(image, decode_chunk_size=8, generator=generator, motion_bucket_id=motion_bucket_id, noise_aug_strength=noise_aug_strength).images[0]
+    frames = self.pipeline(Image.open(image), decode_chunk_size=8, generator=generator, motion_bucket_id=motion_bucket_id, noise_aug_strength=noise_aug_strength).frames[0]
     # TODO: Ensure that these temp files are cleared
     export_to_video(frames, "generated.mp4", fps=7)
     return Path("generated.mp4")
