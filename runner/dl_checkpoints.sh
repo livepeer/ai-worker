fd1219533c694c06b526b79c617e3d5f0958b86d
Yondon Fu <yondon.fu@gmail.com>
2024-02-07 20:39:42 +0000

    runner: Add sdxl-turbo to dl script

--- runner/dl_checkpoints.sh
+++ runner/dl_checkpoints.sh
@@ -4,6 +4,7 @@ mkdir -p models
 
 # text-to-image, image-to-image
 huggingface-cli download stabilityai/sd-turbo --include "*.fp16.safetensors" "*.json" "*.txt" --exclude ".onnx" ".onnx_data" --cache-dir models
+huggingface-cli download stabilityai/sdxl-turbo --include "*.fp16.safetensors" "*.json" "*.txt" --exclude ".onnx" ".onnx_data" --cache-dir models
 huggingface-cli download runwayml/stable-diffusion-v1-5 --include "*.fp16.safetensors" "*.json" "*.txt" --exclude ".onnx" ".onnx_data" --cache-dir models
 huggingface-cli download stabilityai/stable-diffusion-xl-base-1.0 --include "*.fp16.safetensors" "*.json" "*.txt" --exclude ".onnx" ".onnx_data" --cache-dir models
 huggingface-cli download prompthero/openjourney-v4 --include "*.safetensors" "*.json" "*.txt" --exclude ".onnx" ".onnx_data" --cache-dir models
