b6b6add15e383c8cc56d3d47f5a46f79630bc936
John | Elite Encoder <john@eliteencoder.net>
2024-04-25 02:19:56 -0400

    Fix broken example code

--- cmd/examples/text-to-image/main.go
+++ cmd/examples/text-to-image/main.go
@@ -39,7 +39,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if err := w.Warm(ctx, containerName, modelID, ""); err != nil {
+	if err := w.Warm(ctx, containerName, modelID, worker.RunnerEndpoint{}, worker.OptimizationFlags{}); err != nil {
 		slog.Error("Error warming container", slog.String("error", err.Error()))
 		return
 	}
