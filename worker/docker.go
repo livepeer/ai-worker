abd181a5b6e1e298885a7b08db09aeddd049454c
Elite Encoder <john@eliteencoder.net>
2024-06-12 15:20:39 -0400

    Add speech-to-text host container port

--- worker/docker.go
+++ worker/docker.go
@@ -34,6 +34,7 @@ var containerHostPorts = map[string]string{
 	"image-to-image": "8001",
 	"image-to-video": "8002",
 	"upscale":        "8003",
+	"speech-to-text": "8004",
 }
 
 type DockerManager struct {
