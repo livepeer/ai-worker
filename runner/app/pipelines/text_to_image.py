face28d1c8fff075732f16cded1450f0b563a519
Marco van Dijk <marco@stronk.rocks>
2024-07-10 12:21:07 +0200

    `txt2img`: add LoRa support

--- runner/app/pipelines/text_to_image.py
+++ runner/app/pipelines/text_to_image.py
@@ -8,7 +8,7 @@ import torch
 from app.pipelines.base import Pipeline
 from app.pipelines.utils import (SafetyChecker, get_model_dir,
                                  get_torch_device, is_lightning_model,
-                                 is_turbo_model, split_prompt)
+                                 is_turbo_model, split_prompt, load_loras)
 from diffusers import (AutoPipelineForText2Image, EulerDiscreteScheduler,
                        StableDiffusion3Pipeline, StableDiffusionXLPipeline,
                        UNet2DConditionModel, FluxPipeline)
@@ -35,6 +35,7 @@ class ModelName(Enum):
 
 class TextToImagePipeline(Pipeline):
     def __init__(self, model_id: str):
+        self.loaded_loras = ""
         self.model_id = model_id
         kwargs = {"cache_dir": get_model_dir()}
 
@@ -201,6 +202,19 @@ class TextToImagePipeline(Pipeline):
                 kwargs["generator"] = [
                     torch.Generator(get_torch_device()).manual_seed(s) for s in seed
                 ]
+
+        # Dynamically (un)load LoRas. Defaults to "" when not passed, so should always be present in kwargs
+        if self.loaded_loras != kwargs["loras"]:
+            # Unload previously loaded LoRas
+            # NOTE: we might want to keep LoRas loaded and only reset their weights
+            # TODO: run tests with VRAM usage. We should be able to keep the last x LoRas loaded without issues
+            if self.loaded_loras != "":
+                self.ldm.unload_lora_weights()
+            # Remember requested LoRas and their weights
+            self.loaded_loras = kwargs["loras"]
+            load_loras(self.ldm, self.loaded_loras)
+        # Do not pass the lora param to the model when running inference
+        del kwargs["loras"]
 
         if num_inference_steps is None or num_inference_steps < 1:
             del kwargs["num_inference_steps"]
