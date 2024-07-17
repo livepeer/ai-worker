4d2a6afac899d91703cd2e962da4674c582aad3d
Rick Staa <rick.staa@outlook.com>
2024-07-17 13:16:13 +0200

    feat: improve I2V num_inference_steps behavoir
    This commit improves the I2V `num_inference_steps` defaults and code.

--- runner/app/routes/image_to_video.py
+++ runner/app/routes/image_to_video.py
@@ -43,7 +43,9 @@ async def image_to_video(
     noise_aug_strength: Annotated[float, Form()] = 0.02,
     seed: Annotated[int, Form()] = None,
     safety_check: Annotated[bool, Form()] = True,
-    num_inference_steps: Annotated[int, Form()] = 25,  # TODO: Make optional.
+    num_inference_steps: Annotated[
+        int, Form()
+    ] = 25,  # NOTE: Hardcoded due to varying pipeline values.
     pipeline: Pipeline = Depends(get_pipeline),
     token: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
 ):
