328d8a12a3ef843fd8fbd932052c695893a21eba
Rick Staa <rick.staa@outlook.com>
2024-08-01 10:33:53 +0200

    refactor: sort imports using isort (#142)
    This commit sorts the python imports using the
    https://pypi.org/project/isort package.

--- runner/app/pipelines/optim/sfast.py
+++ runner/app/pipelines/optim/sfast.py
@@ -5,7 +5,8 @@ For more information, see the DeepCache project on GitHub: https://github.com/ch
 
 import logging
 
-from sfast.compilers.diffusion_pipeline_compiler import CompilationConfig, compile
+from sfast.compilers.diffusion_pipeline_compiler import (CompilationConfig,
+                                                         compile)
 
 logger = logging.getLogger(__name__)
 
--- runner/app/routes/upscale.py
+++ runner/app/routes/upscale.py
@@ -3,14 +3,14 @@ import os
 import random
 from typing import Annotated
 
+from app.dependencies import get_pipeline
+from app.pipelines.base import Pipeline
+from app.routes.util import (HTTPError, ImageResponse, http_error,
+                             image_to_data_url)
 from fastapi import APIRouter, Depends, File, Form, UploadFile, status
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from PIL import Image, ImageFile
-
-from app.dependencies import get_pipeline
-from app.pipelines.base import Pipeline
-from app.routes.util import HTTPError, ImageResponse, http_error, image_to_data_url
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
