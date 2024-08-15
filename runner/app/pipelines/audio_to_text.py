0e5f6825a82a1926c2003e93f3d8baffbb0466fe
Brad P <brad@ad-astra.video>
2024-08-15 17:43:33 -0500

    enable new models in audio-to-text

--- runner/app/pipelines/audio_to_text.py
+++ runner/app/pipelines/audio_to_text.py
@@ -16,6 +16,8 @@ logger = logging.getLogger(__name__)
 
 MODEL_INCOMPATIBLE_EXTENSIONS = {
     "openai/whisper-large-v3": ["mp4", "m4a", "ac3"],
+    "openai/whisper-medium": ["mp4", "m4a", "ac3"],
+    "distil-whisper/distil-large-v3": ["mp4", "m4a", "ac3"]
 }
 
 
@@ -40,6 +42,10 @@ class AudioToTextPipeline(Pipeline):
 
             kwargs["torch_dtype"] = torch.float16
             kwargs["variant"] = "fp16"
+
+        if os.environ.get("FLOAT16"):
+            logger.info("AudioToTextPipeline using float16 precision for %s", model_id)
+            kwargs["torch_dtype"] = torch.float16
 
         if os.environ.get("BFLOAT16"):
             logger.info("AudioToTextPipeline using bfloat16 precision for %s", model_id)
