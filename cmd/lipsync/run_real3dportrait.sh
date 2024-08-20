ddc9d596aa4e4a6d2b7425b93a262cba12ecfcb7
Peter Schroedl <peter@livepeer.org>
2024-08-20 20:20:52 +0000

    fix variable substitution in run.sh

--- cmd/lipsync/run_real3dportrait.sh
+++ cmd/lipsync/run_real3dportrait.sh
@@ -3,7 +3,7 @@ activate_conda_env() {
   local env_path="$1"
   if [ -f "${env_path}/etc/profile.d/conda.sh" ]; then
     source "${env_path}/etc/profile.d/conda.sh"
-    eval "$($env_path}/bin/conda shell.bash hook)"
+    eval "$(${env_path}/bin/conda shell.bash hook)"
     conda activate real3dportrait
   else
     echo "Conda activation script not found at ${env_path}/etc/profile.d/conda.sh"
@@ -11,18 +11,18 @@ activate_conda_env() {
   fi
 }
 
-activate_conda_env "/models/models--yerfor--Real3DPortrait/anaconda3"
+activate_conda_env "/models--yerfor--Real3DPortrait/anaconda3"
 
 # Debugging: Check which Python is being used - 3.9 is used by the conda env for real3dportrait
 which python
 python --version
 
 # Set PYTHONPATH
-export PYTHONPATH="/models/models--yerfor--Real3DPortrait:$PYTHONPATH"
-cd /models/models--yerfor--Real3DPortrait
+export PYTHONPATH="/models--yerfor--Real3DPortrait/cloned:$PYTHONPATH"
+cd /models--yerfor--Real3DPortrait/cloned
 
 # Run the Real3DPortrait inference command
-python /models/models--yerfor--Real3DPortrait/inference/real3d_infer.py \
+python /models--yerfor--Real3DPortrait/cloned/inference/real3d_infer.py \
   --src_img "$1" \
   --drv_aud "$2" \
   --out_name "$3" \
