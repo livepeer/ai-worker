acd6407aa4e66fa27dccc4e60eec3a66feefb7cc
Rick Staa <rick.staa@outlook.com>
2024-09-03 16:16:16 +0200

    chore(worker): remove debug patch
    This commit removes the debug patch which was accidentally added to the
    code.

--- worker/docker.go
+++ worker/docker.go
@@ -183,7 +183,6 @@ func (m *DockerManager) createContainer(ctx context.Context, pipeline string, mo
 		},
 		ExposedPorts: nat.PortSet{
 			containerPort: struct{}{},
-			"5678/tcp":    struct{}{},
 		},
 		Labels: map[string]string{
 			containerCreatorLabel: containerCreator,
@@ -209,12 +208,6 @@ func (m *DockerManager) createContainer(ctx context.Context, pipeline string, mo
 				{
 					HostIP:   "0.0.0.0",
 					HostPort: containerHostPort,
-				},
-			},
-			"5678/tcp": []nat.PortBinding{
-				{
-					HostIP:   "0.0.0.0",
-					HostPort: "5678",
 				},
 			},
 		},
