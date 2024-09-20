edefccf3e468bfd3d396aa798d91756b029141ee
Jason <83615043+JJassonn69@users.noreply.github.com>
2024-09-20 21:03:56 +0300

    Update docker.go

--- worker/docker.go
+++ worker/docker.go
@@ -32,11 +32,11 @@ const containerCreator = "ai-worker"
 // using the GPU we stop it so we don't have to worry about having enough ports
 var containerHostPorts = map[string]string{
 	"text-to-image":  "8000",
-	"image-to-image": "8100",
-	"image-to-video": "8200",
-	"upscale":        "8300",
+       "image-to-image":  "8100",
+       "image-to-video":  "8200",
+              "upscale":  "8300",
 	"audio-to-text":  "8400",
-  "segment-anything-2": "8500",
+   "segment-anything-2":  "8500",
 	"live-portrait":  "8900",
 }
 
