241cf7a50fe52b216294aceef2f1794339bb6a6c
Brad P <brad@ad-astra.video>
2024-09-23 11:13:43 -0500

    add default container for LLM and fix requirements.txt

--- worker/docker.go
+++ worker/docker.go
@@ -43,6 +43,7 @@ var containerHostPorts = map[string]string{
 // Mapping for per pipeline container images.
 var pipelineToImage = map[string]string{
 	"segment-anything-2": "livepeer/ai-runner:segment-anything-2",
+	"llm-generate": "livepeer/ai-runner:llm"
 }
 
 type DockerManager struct {
