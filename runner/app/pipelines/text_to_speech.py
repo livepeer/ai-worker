f425e7302cf63a08610f09e4de034058b26fc24d
Peter Schroedl <peter_schroedl@me.com>
2024-10-09 21:02:33 +0000

    use model_id param, remove unecessary clearing of memory, whitespace

--- runner/app/pipelines/text_to_speech.py
+++ runner/app/pipelines/text_to_speech.py
@@ -8,8 +8,6 @@ import soundfile as sf
 import os
 import torch
 import logging
-import time
-import gc
 
 logger = logging.getLogger(__name__)
 
@@ -18,18 +16,16 @@ class TextToSpeechPipeline(Pipeline):
         self.device = get_torch_device()
         self.model_id = model_id
         kwargs = {"cache_dir": get_model_dir()}
-        print(f"ModelID : {model_id}, cache_dir: {get_model_dir()}")
+
         self.TTS_model = ParlerTTSForConditionalGeneration.from_pretrained(
-            "parler-tts/parler-tts-large-v1",
-            # model_id,
+            model_id,
             **kwargs,
         ).to(self.device)
 
         self.TTS_tokenizer = AutoTokenizer.from_pretrained(
-            "parler-tts/parler-tts-large-v1",
-            # model_id,
+            model_id,
             torch_dtype=torch.bfloat16,
-            # attn_implementation="flash_attention_2", # TODO: install flash_attn in Dockerfile.tts
+            # attn_implementation="flash_attention_2", # TODO (pschroedl): investigate installing flash_attn + cuda toolkit in Dockerfile.text_to_speech
             **kwargs,
         )
 
@@ -41,41 +37,19 @@ class TextToSpeechPipeline(Pipeline):
         return audio_path
 
     def generate_speech(self, text, tts_steering, output_file_name):
-        try:
-            with torch.no_grad():
-                input_ids = self.TTS_tokenizer(tts_steering, return_tensors="pt").input_ids.to(self.device)
-                prompt_input_ids = self.TTS_tokenizer(text, return_tensors="pt").input_ids.to(self.device)
+        with torch.no_grad():
+            input_ids = self.TTS_tokenizer(tts_steering, return_tensors="pt").input_ids.to(self.device)
+            prompt_input_ids = self.TTS_tokenizer(text, return_tensors="pt").input_ids.to(self.device)
 
-                generation = self.TTS_model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
-                generated_audio = generation.cpu().numpy().squeeze()
+            generation = self.TTS_model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+            generated_audio = generation.cpu().numpy().squeeze()
 
-                sf.write(output_file_name, generated_audio, samplerate=44100)
+            sf.write(output_file_name, generated_audio, samplerate=44100)
 
-                # Free the tensors
-                del input_ids, prompt_input_ids, generation, generated_audio
-        except torch.cuda.OutOfMemoryError:
-            logger.error("CUDA Out of Memory Error during speech generation.")
-            self.cleanup_cuda_memory()
-            raise
-        finally:
-            self.cleanup_cuda_memory()
+            # Free the tensors
+            del input_ids, prompt_input_ids, generation, generated_audio
+
         return output_file_name
-
-    def cleanup_cuda_memory(self):
-        torch.cuda.empty_cache()
-        gc.collect()
-        logger.info("CUDA memory cleared.")
-
-    def unload_model(self, model):
-        # Move all components of the pipeline to CPU if they have the .cpu() method
-        if hasattr(model, 'components'):
-            for component in model.components.values():
-                if hasattr(component, 'cpu'):
-                    component.cpu()
-        del model
-        torch.cuda.empty_cache()
-        gc.collect()
-        time.sleep(20)
 
     def __str__(self) -> str:
         return f"Text-To-Speech model_id={self.model_id}"
