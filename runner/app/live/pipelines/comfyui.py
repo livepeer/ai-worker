d7513cf4d0f3ed6be6da3625e02fe5ad6fbb0139
Rafal Leszko <rafal@livepeer.org>
2024-11-12 09:49:24 +0000

    Allow to update ComfyUI JSON string

--- runner/app/live/pipelines/comfyui.py
+++ runner/app/live/pipelines/comfyui.py
@@ -9,25 +9,53 @@ import numpy as np
 
 from .interface import Pipeline
 
+COMFY_UI_WORKSPACE_ENV = "COMFY_UI_WORKSPACE"
+DEFAULT_WORKFLOW_JSON = '''
+{
+    "1": {
+        "inputs": {
+            "images": ["2", 0]
+        },
+        "class_type": "SaveTensor",
+        "_meta": {
+            "title": "SaveTensor"
+        }
+    },
+    "2": {
+        "inputs": {
+            "engine": "depth_anything_vitl14-fp16.engine",
+            "images": ["3", 0]
+        },
+        "class_type": "DepthAnythingTensorrt",
+        "_meta": {
+            "title": "Depth Anything Tensorrt"
+        }
+    },
+    "3": {
+        "inputs": {},
+        "class_type": "LoadTensor",
+        "_meta": {
+            "title": "LoadTensor"
+        }
+    }
+}
+'''
+
 
 class ComfyUI(Pipeline):
   def __init__(self, **params):
     super().__init__(**params)
-    # configure as is needed
-    comfy_ui_workspace = os.getenv("COMFY_UI_WORKSPACE")
-    default_workflow = os.getenv("COMFY_UI_DEFAULT_WORKFLOW")
 
+    comfy_ui_workspace = os.getenv(COMFY_UI_WORKSPACE_ENV)
     self.client = ComfyStreamClient(cwd=comfy_ui_workspace)
-    with open(default_workflow, "r") as f:
-      prompt = json.load(f)
-    self.client.set_prompt(prompt)
+
+    prompt = json.loads(DEFAULT_WORKFLOW_JSON)
+    self.update_params(**prompt)
 
     # Comfy will cache nodes that only need to be run once (i.e. a node that loads model weights)
     # We can run the prompt once before actual inputs come in to "warmup"
-    # input = torch.randn(1, 512, 512, 3)
-    # self.client.queue_prompt(input)
-
-    # self.update_params(**params)
+    warmup_input = torch.randn(1, 512, 512, 3)
+    asyncio.get_event_loop().run_until_complete(self.client.queue_prompt(warmup_input))
 
   def process_frame(self, image: Image.Image) -> Image.Image:
     # Normalize by dividing by 255 to ensure the tensor values are between 0 and 1
@@ -38,8 +66,7 @@ class ComfyUI(Pipeline):
     image_tensor = torch.tensor(image_np).unsqueeze(0)
 
     # Process using ComfyUI pipeline
-    loop = asyncio.get_event_loop()
-    result_tensor = loop.run_until_complete(self.client.queue_prompt(image_tensor))
+    result_tensor = asyncio.get_event_loop().run_until_complete(self.client.queue_prompt(image_tensor))
 
     # Convert back from Tensor to PIL.Image
     result_tensor = result_tensor.squeeze(0)
@@ -48,6 +75,5 @@ class ComfyUI(Pipeline):
     return result_image
 
   def update_params(self, **params):
-    # Convert params into a Prompt type which describes the workflow
-    # self.client.set_prompt(params["config"])
-    return
+    # params is the JSON string with the ComfyUI workflow
+    self.client.set_prompt(params)
