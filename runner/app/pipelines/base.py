3c4a2e4a54ba178d8c806a1b0752d1ecdad50f66
Victor Elias <victorgelias@gmail.com>
2024-11-14 11:08:50 -0300

    runner: Improve Pipeline.model_id comment

--- runner/app/pipelines/base.py
+++ runner/app/pipelines/base.py
@@ -5,7 +5,7 @@ from typing import Any
 class Pipeline(ABC):
     @abstractmethod
     def __init__(self, model_id: str, model_dir: str):
-        self.model_id: str # type hint so we can use the field in routes
+        self.model_id: str # declare the field here so the type hint is available when using this abstract class
         raise NotImplementedError("Pipeline should implement an __init__ method")
 
     @abstractmethod
