49ea7dfc288f4503a28e0567c33a45397cbd152b
Rick Staa <rick.staa@outlook.com>
2024-11-22 16:51:54 +0100

    test: remove broken Docker tests
    This commit removes the broken Docker tests as the current mocking approach
    is not working correctly. Additionally, it includes the following improvements:

--- go.mod
+++ go.mod
@@ -15,8 +15,8 @@ require (
 )
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
-	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -39,17 +39,16 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.56.0 // indirect
-	go.opentelemetry.io/otel v1.31.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.31.0 // indirect
-	go.opentelemetry.io/otel/metric v1.31.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.31.0 // indirect
-	go.opentelemetry.io/otel/trace v1.31.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.57.0 // indirect
+	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.32.0 // indirect
+	go.opentelemetry.io/otel/metric v1.32.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.32.0 // indirect
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 )
--- worker/docker_test.go
+++ worker/docker_test.go
@@ -1,167 +1,107 @@
 package worker
 
 import (
-	"context"
-	"errors"
-	"io"
-	"strings"
-	"sync"
+	"fmt"
 	"testing"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/api/types/network"
-	docker "github.com/docker/docker/client"
-	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-// MockDockerClient is a mock implementation of the Docker client.
-type MockDockerClient struct {
-	docker.Client
-	mock.Mock
-}
-
-func (m *MockDockerClient) ImagePull(ctx context.Context, ref string, options image.PullOptions) (io.ReadCloser, error) {
-	args := m.Called(ctx, ref, options)
-	return args.Get(0).(io.ReadCloser), args.Error(1)
-}
-
-func (m *MockDockerClient) ImageInspectWithRaw(ctx context.Context, imageID string) (types.ImageInspect, []byte, error) {
-	args := m.Called(ctx, imageID)
-	return args.Get(0).(types.ImageInspect), args.Get(1).([]byte), args.Error(2)
-}
-
-func (m *MockDockerClient) ContainerCreate(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, platform *ocispec.Platform, containerName string) (container.CreateResponse, error) {
-	args := m.Called(ctx, config, hostConfig, networkingConfig, platform, containerName)
-	return args.Get(0).(container.CreateResponse), args.Error(1)
-}
-
-func (m *MockDockerClient) ContainerStart(ctx context.Context, containerID string, options container.StartOptions) error {
-	args := m.Called(ctx, containerID, options)
-	return args.Error(0)
-}
-
-func (m *MockDockerClient) ContainerInspect(ctx context.Context, containerID string) (types.ContainerJSON, error) {
-	args := m.Called(ctx, containerID)
-	return args.Get(0).(types.ContainerJSON), args.Error(1)
-}
-
-func (m *MockDockerClient) ContainerList(ctx context.Context, options container.ListOptions) ([]types.Container, error) {
-	args := m.Called(ctx, options)
-	return args.Get(0).([]types.Container), args.Error(1)
-}
-
-func (m *MockDockerClient) ContainerStop(ctx context.Context, containerID string, options container.StopOptions) error {
-	args := m.Called(ctx, containerID, options)
-	return args.Error(0)
-}
-
-func (m *MockDockerClient) ContainerRemove(ctx context.Context, containerID string, options container.RemoveOptions) error {
-	args := m.Called(ctx, containerID, options)
-	return args.Error(0)
-}
-
-func TestGetContainerImage(t *testing.T) {
-	dockerManager := &DockerManager{
-		defaultImage: "default-image",
-	}
-
+func TestDockerManager_getContainerImageName(t *testing.T) {
 	tests := []struct {
-		pipeline string
-		modelID  string
-		expected string
-		err      bool
+		name          string
+		pipeline      string
+		modelID       string
+		expectedImage string
+		expectError   bool
 	}{
-		{"segment-anything-2", "", "livepeer/ai-runner:segment-anything-2", false},
-		{"text-to-speech", "", "livepeer/ai-runner:text-to-speech", false},
-		{"live-video-to-video", "streamdiffusion", "livepeer/ai-runner:live-app-streamdiffusion", false},
-		{"live-video-to-video", "unknown-model", "", true},
-		{"unknown-pipeline", "", "default-image", false},
+		{
+			name:          "live-video-to-video with valid modelID",
+			pipeline:      "live-video-to-video",
+			modelID:       "streamdiffusion",
+			expectedImage: "livepeer/ai-runner:live-app-streamdiffusion",
+			expectError:   false,
+		},
+		{
+			name:        "live-video-to-video with invalid modelID",
+			pipeline:    "live-video-to-video",
+			modelID:     "invalid-model",
+			expectError: true,
+		},
+		{
+			name:          "valid pipeline",
+			pipeline:      "text-to-speech",
+			modelID:       "",
+			expectedImage: "livepeer/ai-runner:text-to-speech",
+			expectError:   false,
+		},
+		{
+			name:          "invalid pipeline",
+			pipeline:      "invalid-pipeline",
+			modelID:       "",
+			expectedImage: "default-image",
+			expectError:   false,
+		},
 	}
 
 	for _, tt := range tests {
-		image, err := dockerManager.getContainerImage(tt.pipeline, tt.modelID)
-		if tt.err {
-			require.Error(t, err)
-		} else {
-			require.NoError(t, err)
-			require.Equal(t, tt.expected, image)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			manager := &DockerManager{
+				defaultImage: "default-image",
+			}
+
+			image, err := manager.getContainerImageName(tt.pipeline, tt.modelID)
+			if tt.expectError {
+				require.Error(t, err)
+				require.Equal(t, fmt.Sprintf("no container image found for live pipeline %s", tt.modelID), err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expectedImage, image)
+			}
+		})
 	}
 }
 
-func TestPullImageAsync(t *testing.T) {
-	mockDockerClient := new(MockDockerClient)
-	dockerManager := &DockerManager{
-		dockerClient:    mockDockerClient,
-		imagePullStatus: &sync.Map{},
+func TestDockerContainerName(t *testing.T) {
+	tests := []struct {
+		name         string
+		pipeline     string
+		modelID      string
+		suffix       []string
+		expectedName string
+	}{
+		{
+			name:         "with suffix",
+			pipeline:     "text-to-speech",
+			modelID:      "model1",
+			suffix:       []string{"suffix1"},
+			expectedName: "text-to-speech_model1_suffix1",
+		},
+		{
+			name:         "without suffix",
+			pipeline:     "text-to-speech",
+			modelID:      "model1",
+			expectedName: "text-to-speech_model1",
+		},
+		{
+			name:         "modelID with special characters",
+			pipeline:     "text-to-speech",
+			modelID:      "model/1_2",
+			suffix:       []string{"suffix1"},
+			expectedName: "text-to-speech_model-1-2_suffix1",
+		},
+		{
+			name:         "modelID with special characters without suffix",
+			pipeline:     "text-to-speech",
+			modelID:      "model/1_2",
+			expectedName: "text-to-speech_model-1-2",
+		},
 	}
 
-	ctx := context.Background()
-	imageName := "test-image"
-
-	// Mock the ImageInspectWithRaw method to simulate the image being available locally.
-	mockDockerClient.On("ImageInspectWithRaw", ctx, imageName).Return(types.ImageInspect{}, nil, nil)
-
-	// Call pullImageAsync and verify that it does not attempt to pull the image.
-	dockerManager.pullImageAsync(ctx, imageName)
-	mockDockerClient.AssertNotCalled(t, "ImagePull", ctx, imageName, mock.Anything)
-
-	// Mock the ImageInspectWithRaw method to simulate the image not being available locally.
-	mockDockerClient.On("ImageInspectWithRaw", ctx, imageName).Return(types.ImageInspect{}, nil, errors.New("not found"))
-
-	// Mock the ImagePull method to simulate pulling the image.
-	mockDockerClient.On("ImagePull", ctx, imageName, mock.Anything).Return(io.NopCloser(strings.NewReader("")), nil)
-
-	// Call pullImageAsync and verify that it attempts to pull the image.
-	dockerManager.pullImageAsync(ctx, imageName)
-	mockDockerClient.AssertCalled(t, "ImagePull", ctx, imageName, mock.Anything)
-}
-
-func TestPullImagesAtStartup(t *testing.T) {
-	mockDockerClient := new(MockDockerClient)
-	dockerManager := &DockerManager{
-		dockerClient:    mockDockerClient,
-		imagePullStatus: &sync.Map{},
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			name := dockerContainerName(tt.pipeline, tt.modelID, tt.suffix...)
+			require.Equal(t, tt.expectedName, name)
+		})
 	}
-
-	ctx := context.Background()
-
-	// Mock the ImageInspectWithRaw method to simulate the images being available locally.
-	for _, imageName := range pipelineToImage {
-		mockDockerClient.On("ImageInspectWithRaw", ctx, imageName).Return(types.ImageInspect{}, nil, nil)
-	}
-	for _, imageName := range livePipelineToImage {
-		mockDockerClient.On("ImageInspectWithRaw", ctx, imageName).Return(types.ImageInspect{}, nil, nil)
-	}
-
-	// Call pullImagesAtStartup and verify that it does not attempt to pull the images.
-	dockerManager.pullImagesAtStartup(ctx)
-	for _, imageName := range pipelineToImage {
-		mockDockerClient.AssertNotCalled(t, "ImagePull", ctx, imageName, mock.Anything)
-	}
-	for _, imageName := range livePipelineToImage {
-		mockDockerClient.AssertNotCalled(t, "ImagePull", ctx, imageName, mock.Anything)
-	}
-}
-
-func TestPullImage(t *testing.T) {
-	mockDockerClient := new(MockDockerClient)
-	dockerManager := &DockerManager{
-		dockerClient: mockDockerClient,
-	}
-
-	ctx := context.Background()
-	imageName := "test-image"
-
-	// Mock the ImagePull method to simulate pulling the image.
-	mockDockerClient.On("ImagePull", ctx, imageName, mock.Anything).Return(io.NopCloser(strings.NewReader("")), nil)
-
-	// Call pullImage and verify that it pulls the image.
-	err := dockerManager.pullImage(ctx, imageName)
-	require.NoError(t, err)
-	mockDockerClient.AssertCalled(t, "ImagePull", ctx, imageName, mock.Anything)
 }
