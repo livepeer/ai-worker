2661dfa31d91caaf0caa900e7b08782d5e9afedf
Elite Encoder <john@eliteencoder.net>
2024-11-23 09:32:15 +0000

    get trickle server url from runner

--- runner/app/routes/live_video_to_video.py
+++ runner/app/routes/live_video_to_video.py
@@ -12,10 +12,11 @@ from app.routes.utils import (
     http_error,
     handle_pipeline_exception,
 )
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel, Field
+from urllib.parse import urlparse
 
 router = APIRouter()
 
@@ -96,6 +97,7 @@ RESPONSES: dict[int | str, dict[str, Any]]= {
     include_in_schema=False,
 )
 async def live_video_to_video(
+    request: Request,
     params: LiveVideoToVideoParams,
     pipeline: Pipeline = Depends(get_pipeline),
     token: HTTPAuthorizationCredentials = Depends(HTTPBearer(auto_error=False)),
@@ -117,6 +119,13 @@ async def live_video_to_video(
                 f"{params.model_id}."
             ),
         )
+
+    if "trickle_port" in params.params:
+        trickle_port = params.params["trickle_port"]
+        params.subscribe_url = urlparse(params.subscribe_url)._replace(scheme="https", netloc=f"{request.client.host}:{trickle_port}").geturl()
+        params.publish_url = urlparse(params.publish_url)._replace(scheme="https", netloc=f"{request.client.host}:{trickle_port}").geturl()
+        params.control_url = urlparse(params.control_url)._replace(scheme="https", netloc=f"{request.client.host}:{trickle_port}").geturl()
+        del params.params["trickle_port"]
 
     try:
         pipeline(**params.model_dump())
