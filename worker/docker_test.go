dfe2b12c6eea69d634a235e6c0f89293c06099b0
RUFFY-369 <prakarshkaushik369@gmail.com>
2024-12-02 14:27:12 +0530

    test:add regular string input case

--- worker/docker_test.go
+++ worker/docker_test.go
@@ -1032,6 +1032,13 @@ func TestDockerManager_overridePipelineImages(t *testing.T) {
 			pipeline:    "segment-anything-2",
 			expectError: true,
 		},
+		{
+			name:          "RegularStringInput",
+			inputJSON:     "",
+			pipeline:      "image-to-video",
+			expectedImage: "default-image",
+			expectError:   false,
+		},
 	}
 
 	for _, tt := range tests {
