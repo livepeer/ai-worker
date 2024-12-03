4f70cc4085c345564e53b49aa107ceb9cd851912
Elite Encoder <john@eliteencoder.net>
2024-12-03 06:25:33 +0000

    load model checkpoint from request

--- runner/app/live/Sam2Wrapper/wrapper.py
+++ runner/app/live/Sam2Wrapper/wrapper.py
@@ -7,6 +7,7 @@ from omegaconf import OmegaConf
 from hydra.utils import instantiate
 from hydra import initialize_config_dir, compose
 from hydra.core.global_hydra import GlobalHydra
+import os
 
 MODEL_MAPPING = {
     "facebook/sam2-hiera-tiny": {
@@ -35,10 +36,6 @@ if torch.cuda.get_device_properties(0).major >= 8:
 # Initialize Hydra to load the configuration
 if GlobalHydra.instance().is_initialized():
     GlobalHydra.instance().clear()
-    
-config_path = "/workspaces/ai-worker/runner/models/sam2_configs"
-sam2_checkpoint = "/models/checkpoints/sam2_hiera_tiny.pt"
-model_cfg = "sam2_hiera_t.yaml"
 
 class Sam2Wrapper:
     def __init__(
@@ -47,10 +44,17 @@ class Sam2Wrapper:
         device: Optional[str] = None,
         **kwargs
     ):
-        self.model_id = model_id_or_path
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
-
-        # Code ripped out of sam2.build_sam.build_sam2_camera_predictor to appease Hydra
+        self.model_id = model_id_or_path
+        if model_id_or_path in MODEL_MAPPING:
+            model_info = MODEL_MAPPING[model_id_or_path]
+            model_cfg = model_info['config']
+            config_path = os.path.abspath((f"models/sam2_configs"))
+            sam2_checkpoint = f"/models/checkpoints/{model_info['checkpoint']}"
+        else:
+            raise ValueError(f"Model ID {model_id_or_path} not supported")
+        
+        # Code from sam2.build_sam.build_sam2_camera_predictor to appease Hydra
         with initialize_config_dir(config_dir=config_path, version_base=None):
             cfg = compose(config_name=model_cfg)
 
@@ -100,12 +104,5 @@ def load_checkpoint(model, ckpt_path, device):
         if unexpected_keys:
             logging.error(f"Unexpected keys: {unexpected_keys}")
             raise RuntimeError("Unexpected keys while loading checkpoint.")
-        logging.info("Loaded checkpoint successfully.")
-
-def get_torch_device():
-    if torch.cuda.is_available():
-        return torch.device("cuda")
-    elif torch.backends.mps.is_available():
-        return torch.device("mps")
-    else:
-        return torch.device("cpu")
+        logging.info(f"Loaded checkpoint successfully: {ckpt_path}")
+    
