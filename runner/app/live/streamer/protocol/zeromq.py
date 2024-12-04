c293b59e3de5b1bb5b634d6211eb5a1cdde0a63c
Victor Elias <victor@livepeer.org>
2024-12-04 19:24:01 +0000

    runner/live: Plug protocols by composition instead
    The inheritance was so ugly and got in the way several times.

--- runner/app/live/streamer/protocol/zeromq.py
+++ runner/app/live/streamer/protocol/zeromq.py
@@ -5,41 +5,26 @@ from PIL import Image
 from multiprocessing.synchronize import Event
 from typing import AsyncGenerator
 
-from .streamer import PipelineStreamer
+from .protocol import StreamProtocol
 from .jpeg import to_jpeg_bytes, from_jpeg_bytes
 
 
-class ZeroMQStreamer(PipelineStreamer):
-    def __init__(
-        self,
-        input_address: str,
-        output_address: str,
-        pipeline: str,
-        input_timeout: int,
-        params: dict,
-    ):
-        super().__init__(pipeline, input_timeout, params)
+class ZeroMQProtocol(StreamProtocol):
+    def __init__(self, input_address: str, output_address: str):
         self.input_address = input_address
         self.output_address = output_address
-
         self.context = zmq.asyncio.Context()
         self.input_socket = self.context.socket(zmq.SUB)
         self.output_socket = self.context.socket(zmq.PUB)
 
     def start(self):
         self.input_socket.connect(self.input_address)
-        self.input_socket.setsockopt_string(
-            zmq.SUBSCRIBE, ""
-        )  # Subscribe to all messages
+        self.input_socket.setsockopt_string(zmq.SUBSCRIBE, "")
         self.input_socket.set_hwm(10)
-
         self.output_socket.connect(self.output_address)
         self.output_socket.set_hwm(10)
 
-        super().start()
-
     async def stop(self):
-        await super().stop()
         self.input_socket.close()
         self.output_socket.close()
         self.context.term()
