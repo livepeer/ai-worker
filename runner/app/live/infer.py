ad71705992009e59b4c4210c283538f3b1e285c6
Peter Schroedl <peter_schroedl@me.com>
2024-12-06 16:52:56 +0000

    remove changes to infer.py, remove unused pipeline

--- runner/app/live/infer.py
+++ runner/app/live/infer.py
@@ -17,26 +17,32 @@ infer_root = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, infer_root)
 
 from params_api import start_http_server
-from streamer.trickle import TrickleStreamer
-from streamer.zeromq import ZeroMQStreamer
+from streamer.protocol.trickle import TrickleProtocol
+from streamer.protocol.zeromq import ZeroMQProtocol
 
 
 async def main(http_port: int, stream_protocol: str, subscribe_url: str, publish_url: str, control_url: str, pipeline: str, params: dict, input_timeout: int):
     if stream_protocol == "trickle":
-        handler = TrickleStreamer(subscribe_url, publish_url, pipeline, input_timeout, params or {})
+        protocol = TrickleProtocol(subscribe_url, publish_url)
     elif stream_protocol == "zeromq":
-        handler = ZeroMQStreamer(subscribe_url, publish_url, pipeline, input_timeout, params or {})
+        protocol = ZeroMQProtocol(subscribe_url, publish_url)
     else:
         raise ValueError(f"Unsupported protocol: {stream_protocol}")
 
+    streamer = PipelineStreamer(protocol, pipeline, input_timeout, params or {})
+
     runner = None
     try:
-        handler.start()
-        runner = await start_http_server(handler, http_port)
-        signal_task = asyncio.create_task(block_until_signal([signal.SIGINT, signal.SIGTERM]))
-        handler_task = asyncio.create_task(start_control_subscriber(handler.wait(), control_url))
+        await streamer.start()
+        runner = await start_http_server(streamer, http_port)
 
-        await asyncio.wait([signal_task, handler_task],
+        tasks: List[asyncio.Task] = []
+        tasks.append(streamer.wait())
+        tasks.append(asyncio.create_task(block_until_signal([signal.SIGINT, signal.SIGTERM])))
+        if control_url is not None and control_url.strip() != "":
+            tasks.append(asyncio.create_task(start_control_subscriber(streamer, control_url)))
+
+        await asyncio.wait(tasks,
             return_when=asyncio.FIRST_COMPLETED
         )
     except Exception as e:
@@ -45,7 +51,7 @@ async def main(http_port: int, stream_protocol: str, subscribe_url: str, publish
         raise e
     finally:
         await runner.cleanup()
-        await handler.stop()
+        await streamer.stop()
 
 
 async def block_until_signal(sigs: List[signal.Signals]):
@@ -70,9 +76,20 @@ async def start_control_subscriber(handler: PipelineStreamer, control_url: str):
         segment = await subscriber.next()
         if segment.eos():
             return
-        params = await segment.read()
-        logging.info("Received control message, updating model with params: %s", params)
-        handler.update_params(**json.loads(params))
+
+        try:
+            params = await segment.read()
+            logging.info("Received control message, updating model with params: %s", params)
+            data = json.loads(params)
+        except Exception as e:
+            logging.error(f"Error parsing control message: {e}")
+            continue
+
+        try:
+            handler.update_params(data)
+        except Exception as e:
+            logging.error(f"Error updating model with control message: {e}")
+            continue
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Infer process to run the AI pipeline")
