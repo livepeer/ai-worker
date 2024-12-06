9f8ad7ac25bcbf87f4e9d707a5db78c024fb8698
Victor Elias <victor@livepeer.org>
2024-12-06 00:39:58 +0000

    trickle: Use trickle publisher directly for events

--- runner/app/live/trickle/__init__.py
+++ runner/app/live/trickle/__init__.py
@@ -1,4 +1,5 @@
 from .media import run_subscribe, run_publish
 from .trickle_subscriber import TrickleSubscriber
+from .trickle_publisher import TricklePublisher
 
-__all__ = ["run_subscribe", "run_publish", "TrickleSubscriber"]
+__all__ = ["run_subscribe", "run_publish", "TrickleSubscriber", "TricklePublisher"]
