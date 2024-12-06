4bce4010487d7195ee0afb9c800bc31e6b41180f
Victor Elias <victor@livepeer.org>
2024-12-06 00:39:58 +0000

    trickle: Use trickle publisher directly for events

--- runner/app/live/streamer/protocol/trickle.py
+++ runner/app/live/streamer/protocol/trickle.py
@@ -7,7 +7,7 @@ from typing import AsyncGenerator, Optional
 from PIL import Image
 from multiprocessing.synchronize import Event
 
-from trickle import media
+from trickle import media, TricklePublisher
 
 from .protocol import StreamProtocol
 from .jpeg import to_jpeg_bytes, from_jpeg_bytes
@@ -19,10 +19,9 @@ class TrickleProtocol(StreamProtocol):
         self.events_url = events_url
         self.subscribe_queue = queue.Queue[bytearray]()
         self.publish_queue = queue.Queue[bytearray]()
-        self.events_queue = queue.Queue[bytearray]() if events_url else None
+        self.events_publisher = None
         self.subscribe_task = None
         self.publish_task = None
-        self.events_task = None
 
     async def start(self):
         self.subscribe_task = asyncio.create_task(
@@ -31,37 +30,29 @@ class TrickleProtocol(StreamProtocol):
         self.publish_task = asyncio.create_task(
             media.run_publish(self.publish_url, self.publish_queue.get)
         )
-        if self.events_url and self.events_queue:
-            self.events_task = asyncio.create_task(
-                # TODO: Update trickle to support simple JSON publishing
-                media.run_publish(self.events_url, self.events_queue.get)
-            )
+        if self.events_url:
+            self.events_publisher = TricklePublisher(self.events_url, "application/json")
 
     async def stop(self):
         if not self.subscribe_task or not self.publish_task:
             return
 
-        # send sentinel None values to stop the trickle tasks
+        # send sentinel None values to stop the trickle tasks gracefully
         self.subscribe_queue.put(None)
         self.publish_queue.put(None)
-        if self.events_queue:
-            self.events_queue.put(None)
+
+        await self.events_publisher.close()
+        self.events_publisher = None
 
         tasks = [self.subscribe_task, self.publish_task]
-        if self.events_task:
-            tasks.append(self.events_task)
-
         try:
             await asyncio.wait(tasks, timeout=30.0)
         except asyncio.TimeoutError:
             for task in tasks:
                 task.cancel()
-        except Exception:
-            logging.error("Error stopping trickle streamer", exc_info=True)
 
         self.subscribe_task = None
         self.publish_task = None
-        self.events_task = None
 
     async def ingress_loop(self, done: Event) -> AsyncGenerator[Image.Image, None]:
         def dequeue_jpeg():
@@ -89,10 +80,11 @@ class TrickleProtocol(StreamProtocol):
             await asyncio.to_thread(enqueue_bytes, frame)
 
     async def report_status(self, status: dict):
-        if not self.events_queue:
+        if not self.events_publisher:
             return
         try:
             status_json = json.dumps(status)
-            self.events_queue.put(status_json.encode())
+            async with await self.events_publisher.next() as event:
+                event.write(status_json.encode())
         except Exception as e:
             logging.error(f"Error reporting status: {e}")
