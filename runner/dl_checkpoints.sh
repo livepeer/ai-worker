54941e3409f0414ec10d4005c62f9ae6067367e2
Paweł Wilczyński <pawel@livepeer.org>
2024-12-12 16:32:52 +0100

    dl_checkpoints.sh new --batch flag to install only sam2-hiera-large model (#358)

--- runner/dl_checkpoints.sh
+++ runner/dl_checkpoints.sh
@@ -15,6 +15,9 @@ function display_help() {
     echo "Options:"
     echo "  --beta  Download beta models."
     echo "  --restricted  Download models with a restrictive license."
+    echo "  --live  Download models only for the livestreaming pipelines."
+    echo "  --tensorrt  Download livestreaming models and build tensorrt models."
+    echo "  --batch  Download all models for batch processing."
     echo "  --help   Display this help message."
 }
 
@@ -164,6 +167,12 @@ function download_restricted_models() {
 
 }
 
+function download_batch_models() {
+    printf "\nDownloading Batch models...\n"
+
+    huggingface-cli download facebook/sam2-hiera-large --include "*.pt" "*.yaml" --cache-dir models
+}
+
 # Enable HF transfer acceleration.
 # See: https://huggingface.co/docs/huggingface_hub/v0.22.1/package_reference/environment_variables#hfhubenablehftransfer.
 export HF_HUB_ENABLE_HF_TRANSFER=1
@@ -190,6 +199,10 @@ do
         ;;
         --tensorrt)
             MODE="tensorrt"
+            shift
+        ;;
+        --batch)
+            MODE="batch"
             shift
         ;;
         --help)
@@ -223,6 +236,8 @@ elif [ "$MODE" = "live" ]; then
     download_live_models
 elif [ "$MODE" = "tensorrt" ]; then
     build_tensorrt_models
+elif [ "$MODE" = "batch" ]; then
+    download_batch_models
 else
     download_all_models
 fi
