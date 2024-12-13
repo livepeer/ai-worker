596ffb558aed40861d666a0b37d9da55d60ac35a
Rafał Leszko <rafal@livepeer.org>
2024-12-13 09:35:58 +0100

    Fix model cache directory (#365)

--- runner/dl_checkpoints.sh
+++ runner/dl_checkpoints.sh
@@ -98,14 +98,14 @@ function download_live_models() {
 }
 
 function download_sam2_checkpoints() {
-    huggingface-cli download facebook/sam2-hiera-tiny --local-dir models/models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-tiny
-    huggingface-cli download facebook/sam2-hiera-small --local-dir models/models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-small
-    huggingface-cli download facebook/sam2-hiera-large --local-dir models/models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-large
+    huggingface-cli download facebook/sam2-hiera-tiny --local-dir models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-tiny
+    huggingface-cli download facebook/sam2-hiera-small --local-dir models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-small
+    huggingface-cli download facebook/sam2-hiera-large --local-dir models/ComfyUI--models/sam2--checkpoints/facebook--sam2-hiera-large
 }
 
 function download_stable_diffusion_checkpoints() {
-    huggingface-cli download KBlueLeaf/kohaku-v2.1 --local-dir models/models/ComfyUI--models/checkpoints --include "*.safetensors"
-    huggingface-cli download stabilityai/sd-turbo --local-dir models/models/ComfyUI--models/checkpoints --include "*.safetensors"
+    huggingface-cli download KBlueLeaf/kohaku-v2.1 --local-dir models/ComfyUI--models/checkpoints --include "*.safetensors"
+    huggingface-cli download stabilityai/sd-turbo --local-dir models/ComfyUI--models/checkpoints --include "*.safetensors"
 }
 
 function build_tensorrt_models() {
