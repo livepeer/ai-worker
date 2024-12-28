11dec17525ac8e05c9749568c042732ccc7ba891
kyriediculous <vergauwennico@gmail.com>
2024-12-28 13:33:14 +0100

    feat: enable fully external configuration

--- cmd/examples/image-to-video/main.go
+++ cmd/examples/image-to-video/main.go
@@ -33,7 +33,9 @@ func main() {
 
 	modelID := "stabilityai/stable-video-diffusion-img2vid-xt"
 
-	w, err := worker.NewWorker(containerImageID, gpus, modelsDir)
+	external := false
+
+	w, err := worker.NewWorker(containerImageID, gpus, modelsDir, external)
 	if err != nil {
 		slog.Error("Error creating worker", slog.String("error", err.Error()))
 		return
