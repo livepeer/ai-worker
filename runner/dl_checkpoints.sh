24f4588ea64e3ea3241ed71dad503c8c58ea0aa7
Peter Schroedl <peter_schroedl@me.com>
2025-01-30 14:36:22 -0800

    provide full name for output engine, bump comfystream commit

--- runner/dl_checkpoints.sh
+++ runner/dl_checkpoints.sh
@@ -105,8 +105,8 @@ function download_live_models() {
                  source /miniconda3/etc/profile.d/conda.sh && conda activate comfystream && \
                  export PYTHONPATH=\"/ComfyUI:$PYTHONPATH\" && \    
                  python /workspace/src/comfystream/scripts/build_trt.py \
-                --model /models/ComfyUI--models/checkpoints/SD1.5/dreamshaper-8.safetensors \
-                --out-engine /models/ComfyUI--models/tensorrt/" \
+                --model /models/ComfyUI--models/unet/dreamshaper-8-dmd-1kstep.safetensors \
+                --out-engine /models/ComfyUI--models/tensorrt/static-dreamshaper8_SD15_$stat-b-1-h-512-w-512_00001_.engine" \
         || (echo "failed ComfyUI build_trt.py"; return 1)
 }
 
