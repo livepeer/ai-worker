ab949e67032725358b4e264457ee9816cd1e0255
Peter Schroedl <peter_schroedl@me.com>
2025-01-31 13:14:26 -0800

    escape  to match ComfyUI generated trt engine naming

--- runner/dl_checkpoints.sh
+++ runner/dl_checkpoints.sh
@@ -104,7 +104,7 @@ function download_live_models() {
         bash -c "source /miniconda3/etc/profile.d/conda.sh && conda activate comfystream && \
                  python /workspace/src/comfystream/scripts/build_trt.py \
                 --model /models/ComfyUI--models/unet/dreamshaper-8-dmd-1kstep.safetensors \
-                --out-engine /models/ComfyUI--models/tensorrt/static-dreamshaper8_SD15_$stat-b-1-h-512-w-512_00001_.engine" \
+                --out-engine /models/ComfyUI--models/tensorrt/static-dreamshaper8_SD15_\$stat-b-1-h-512-w-512_00001_.engine" \
         || (echo "failed ComfyUI build_trt.py"; return 1)
 }
 
